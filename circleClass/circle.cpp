6e5e6106594c66775cfc3c3f0e886d2675688607
ElvisRodriguez <elvis.rodriguez1@student.mec.cuny.edu>
2018-03-06 12:13:56 -0500

    Assignment Complete

---
+++ circleClass/circle.cpp
@@ -0,0 +1,27 @@
+#include "circle.cc"
+
+//#define NDEBUG
+
+int main() {
+  Circle c;
+
+  assert(c.getCenter_x() == 0);
+  assert(c.getCenter_y() == 0);
+
+  c.changeCenter(2.5, 3.6);
+
+  assert(c.getCenter_x() == 2.5);
+  assert(c.getCenter_y() == 3.6);
+
+  c.setRadius(4);
+  assert(c.getRadius() == 4);
+
+  c.setDiameter();
+  assert(c.getDiameter() == 8);
+
+  assert(c.area() == (PI * ( pow( c.getRadius(), 2 )) ) );
+
+  assert(c.circumference() == 2 * PI * c.getRadius() );
+
+  return 0;
+}
