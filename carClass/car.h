d2a46846681ccbd31216b47e92630ed1dec108cc
ElvisRodriguez <elvis.rodriguez1@student.mec.cuny.edu>
2018-03-08 12:31:24 -0500

    Assignment complete

---
+++ carClass/car.h
@@ -0,0 +1,31 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <iostream>
+#include <string>
+//#define NDEBUG
+#include <cassert>
+using namespace std;
+
+#define SIZE 4
+
+class Car {
+  private:
+    double speed;
+    int index;
+    //Used to 'point' at the directions array
+    string directions = "NESW";
+    //N = North, E = East, S = South, W = West
+    char direction;
+    //Will be an index of the directions array
+  public:
+    Car();
+    Car(double, char);
+    double getSpeed() const;
+    char getDirection() const;
+    void changeDirection(char);
+    void changeSpeed(double);
+    bool isGoingForward() const;
+};
+
+#endif //CAR_H
